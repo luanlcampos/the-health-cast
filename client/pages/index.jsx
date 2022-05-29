5faa3b35d86f7be90c9464247fb8d9cba6b31ad3
luanlcampos <luanlcampos15@gmail.com>
2022-05-29 17:54:08 -0400

    add loading logic to wait the user data to be loaded

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -3,13 +3,22 @@ import Header from "../components/Header";
 import { useAuth } from "../firebase/auth";
 import UnsignedHome from "../components/UnsignedHome";
 import SignedHome from "../components/SignedHome";
+import { AiOutlineLoading } from "react-icons/ai";
 
 export default function Home() {
-  const { user, logout } = useAuth();
+  const { user, userData, adminData, userIsLoading, logout } = useAuth();
+
+  if (user && !userData && !adminData) {
+    return (
+      <div className="loading-page h-screen w-screen flex items-center justify-center">
+        <AiOutlineLoading className="loading-spinner text-3xl" />
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen ">
-      <Header user={user} />
+      <Header />
       {!user ? <UnsignedHome /> : <SignedHome />}
     </div>
   );
