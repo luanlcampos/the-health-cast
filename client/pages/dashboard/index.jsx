c90c0dad34ba8faae2d3999354de7c17135511b3
Supriya Mutharasan <smutharasan@myseneca.ca>
2022-06-03 11:56:38 -0400

    updated the dasbboard component

---
+++ client/pages/dashboard/index.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Header from "@/components/Layout/Header";
+//import ManageLiveSessionsIndex from "@/components/Dashboard/ManageLiveSessions/ManageLiveSessions";
+import Dashboard from "@/components/Dashboard/Dashboard";
+import { useAuth } from "@/firebase/auth";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Loading from "@/components/Loading";
+
+import { v4 as uuidv4 } from "uuid";
+
+export default function DashboardPage() {
+  // obtaining user info from AuthProvider
+  const { user, userData } = useAuth();
+
+  // obtaining Live session ID
+  const router = useRouter();
+  const givenLiveSessionID = router.query.id;
+
+  // redirect user back to the login page
+  if (!user) {
+    window.location.href = "/login";
+  } else if (!userData) {
+    return <Loading />;
+  } else if (userData.isHcp === false) {
+    window.location.href = "/login";
+  } else if (userData.isHcp === true) {
+    console.log("Securely Can access the page");
+  }
+
+  return (
+    <>
+      <Header user={user} />
+      <Dashboard></Dashboard>
+    </>
+  );
+}
