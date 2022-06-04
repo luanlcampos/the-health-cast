68d38265a2366a6c7249fcd9a11ea16d8e8bc0e0
trangntt-016 <trangntt.016@gmail.com>
2022-06-04 15:23:08 -0400

    loading messages on both side

--- client/components/Chat/ChatWrapper.jsx
+++ client/components/Chat/ChatWrapper.jsx
@@ -16,6 +16,7 @@ export default function ChatWrapper({currentUser, selectedProfile}){
     const [chatMessages, setChatMessages] = useState([]);
     const chatBox = useRef(null);
 
+    let count = 0;
     useEffect(()=>{
         if(selectedProfile.email!=null){
             const getMessages = async () => { 
@@ -29,8 +30,8 @@ export default function ChatWrapper({currentUser, selectedProfile}){
             };
             getMessages();
         }
-
-    }, [selectedProfile])
+        // collection(db, "chats") loading multiple times
+    }, [selectedProfile, collection(db, "chats")])
 
     useEffect(() => {
         console.log("new messages");
@@ -97,17 +98,7 @@ export default function ChatWrapper({currentUser, selectedProfile}){
                             <ChatMessage message = {text} time={timestamp} senderEmail = {senderEmail} currentUserEmail = {currentUser.email}/>
                         ))
                     }
-                    {/* <div className="flex justify-end">
-                        <div
-                            className="single-message rounded-tl-lg rounded-bl-lg text-gray-200 rounded-br-lg user mb-4 px-4 py-2">Hey!
-                            Thought I'd reach out to say how are you? 😊
-                        </div>
-                        <span><svg className="user-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 13"
-                                   width="8" height="13"><path opacity=".13"
-                                                               d="M5.188 1H0v11.193l6.467-8.625C7.526 2.156 6.958 1 5.188 1z"></path><path
-                            fill="currentColor"
-                            d="M5.188 0H0v11.193l6.467-8.625C7.526 1.156 6.958 0 5.188 0z"></path></svg></span>
-                    </div> */}
+
 
                 </div>
                 <div className="main-footer text-gray-400">
