f3166f54a11044b351f257d3b89829165d72d415
trangntt-016 <trangntt.016@gmail.com>
2022-06-17 03:30:02 -0400

    update with luan

--- client/components/Chat/Sidebar.jsx
+++ client/components/Chat/Sidebar.jsx
@@ -12,6 +12,15 @@ function Sidebar({currentUserFName, currentUserEmail, handleSetAllUsers, handleS
     const [searchInput, setSearchInput] = useState("");
     const [userChat, setUserChat] = useState();
 
+    const data = [{id: 0, label: "Istanbul, TR (AHL)"}, {id: 1, label: "Paris, FR (CDG)"}];
+    const [isOpen, setOpen] = useState(false);
+    const [items, setItem] = useState(data);
+    const [selectedItem, setSelectedItem] = useState(null);
+    
+    const toggleDropdown = () => setOpen(!isOpen);
+    
+    const handleItemClick = (id) => {
+      selectedItem == id ? setSelectedItem(null) : setSelectedItem(id);}
     
     useEffect(() => {
         const getAllUsers = async () => {          
@@ -99,6 +108,20 @@ function Sidebar({currentUserFName, currentUserEmail, handleSetAllUsers, handleS
                               </div>
                           </form>
                       </div>
+                      <div className='dropdown'>
+                        <div className='dropdown-header' onClick={toggleDropdown}>
+                          {selectedItem ? items.find(item => item.id == selectedItem).label : "Select your destination"}
+                          <i className={`fa fa-chevron-right icon ${isOpen && "open"}`}></i>
+                        </div>
+                        <div className={`dropdown-body ${isOpen && 'open'}`}>
+                          {items.map(item => (
+                            <div className="dropdown-item" onClick={e => handleItemClick(e.target.id)} id={item.id}>
+                              <span className={`dropdown-item-dot ${item.id == selectedItem && 'selected'}`}>• </span>
+                              {item.label}
+                            </div>
+                          ))}
+                        </div>
+                    </div>
                   </div>
                   <div className="aside-messages">
                       {
