ae2a88ca17f7144b3441862bd0b5dd17bed740ec
luanlcampos <luanlcampos15@gmail.com>
2022-06-22 19:23:11 -0400

    fix auto load hcp and reports list

--- client/components/Admin/ReportTable.jsx
+++ client/components/Admin/ReportTable.jsx
@@ -2,10 +2,19 @@ import { useState, useEffect } from "react";
 import { AiOutlineEdit, AiFillDelete, AiOutlineReload } from "react-icons/ai";
 import { FaRegShareSquare } from "react-icons/fa";
 import Select from "react-select";
-import { doc, getDoc, updateDoc, deleteDoc, collection, query, where, getDocs } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  updateDoc,
+  deleteDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 
 import "../../styles/Admin.module.scss";
-import { db } from '@/firebase/clientApp';
+import { db } from "@/firebase/clientApp";
 import Swal from "sweetalert2";
 
 const options = [
@@ -38,41 +47,49 @@ export default function ReportTable({ user }) {
   };
 
   const getReportList = async () => {
-    try {    
-        setReportList([]);
-        const reportsRef = collection(db, "reports");
-        // console.log(`admin id: ${user.uid}\nreportsRef ${JSON.stringify(reportsRef)}`);
-        const q = await query(reportsRef, where("reportedAccountOrg", "==", String(user.uid)));
+    try {
+      setReportList([]);
+      const reportsRef = collection(db, "reports");
+      // console.log(`admin id: ${user.uid}\nreportsRef ${JSON.stringify(reportsRef)}`);
+      const q = await query(
+        reportsRef,
+        where("reportedAccountOrg", "==", String(user.uid))
+      );
 
-        // const usersRef = collection(db,"users");
-        // console.log(`queries: ${JSON.stringify(q)}`);
-        const reportsSnap = await getDocs(q);
-        
-        let i = 0;
-        const reportList = reportsSnap.docs.map((report) => ({ //array of reports for given admin (user.uid)
-            ...report.data(),
-            id: report.id,
-          }));
-        
-        const reportedAccts = [];
-        for (let i = 0; i < reportList.length; i++){
-            const acc = await getDoc(doc(db, "users", String(reportList[i].reportedAccountId)));
-            reportedAccts.push(acc.data());
-        }
-        console.log(`after forloop: ${JSON.stringify(reportedAccts)}`);
-        i = 0;
-        reportList.forEach(rep=>{
-            rep.permission = reportedAccts[i].permission;
-            rep.firstName = reportedAccts[i].firstName;
-            rep.lastName = reportedAccts[i++].lastName;
-        })
+      // const usersRef = collection(db,"users");
+      // console.log(`queries: ${JSON.stringify(q)}`);
+      const reportsSnap = await getDocs(q);
 
-        console.log(`after all operations, displaying reports of org inside reportList.map:`)
-        reportList.map((report)=>{
-            console.log(report);
-        });
+      let i = 0;
+      const reportList = reportsSnap.docs.map((report) => ({
+        //array of reports for given admin (user.uid)
+        ...report.data(),
+        id: report.id,
+      }));
 
-        setReportList(reportList);
+      const reportedAccts = [];
+      for (let i = 0; i < reportList.length; i++) {
+        const acc = await getDoc(
+          doc(db, "users", String(reportList[i].reportedAccountId))
+        );
+        reportedAccts.push(acc.data());
+      }
+      console.log(`after forloop: ${JSON.stringify(reportedAccts)}`);
+      i = 0;
+      reportList.forEach((rep) => {
+        rep.permission = reportedAccts[i].permission;
+        rep.firstName = reportedAccts[i].firstName;
+        rep.lastName = reportedAccts[i++].lastName;
+      });
+
+      console.log(
+        `after all operations, displaying reports of org inside reportList.map:`
+      );
+      reportList.map((report) => {
+        console.log(report);
+      });
+
+      setReportList(reportList);
     } catch (error) {
       console.warn(error);
     }
@@ -80,37 +97,44 @@ export default function ReportTable({ user }) {
 
   const removeHcpFromList = async (e, report) => {
     e.preventDefault();
-    console.log(`removing report ${report.id} from ${report.reportedAccountId} -- hcps; orgId ${user.uid}`)
-    openModal("The selected report will be unavailable once deleted from your list").then(
-      async (result) => {
-        if (result.value) {
-          try {
-            await deleteDoc(doc(db, "reports", report.id));
-            await getReportList();
-            setReqLoading(false);
-          } catch (error) {
-            console.log("Admin table remove report Error", error);
-          }
-        } else {
-          return;
-        }
-      }
+    console.log(
+      `removing report ${report.id} from ${report.reportedAccountId} -- hcps; orgId ${user.uid}`
     );
+    openModal(
+      "The selected report will be unavailable once deleted from your list"
+    ).then(async (result) => {
+      if (result.value) {
+        try {
+          await deleteDoc(doc(db, "reports", report.id));
+          await getReportList();
+          setReqLoading(false);
+        } catch (error) {
+          console.log("Admin table remove report Error", error);
+        }
+      } else {
+        return;
+      }
+    });
   };
 
   const handlePermissionChange = async (e, report) => {
     console.log(`updating permissions`);
-    const hcp = await getDoc(doc(db, "users", String(report.reportedAccountId)));
+    const hcp = await getDoc(
+      doc(db, "users", String(report.reportedAccountId))
+    );
     hcp = hcp.data();
     // console.log(`after querying hcp: ${JSON.stringify(hcp.permission.toLowerCase())} && e.value: ${e.value.toLowerCase()}`)
     if (hcp.permission.toLowerCase() !== e.value.toLowerCase()) {
       openModal("The user permissions will be updated").then(async (result) => {
         if (result.isConfirmed) {
           try {
-            console.log(`in updateDoc permissions`)
-            await updateDoc(doc(db, "users", String(report.reportedAccountId)), {
-              permission: e.value,
-            });
+            console.log(`in updateDoc permissions`);
+            await updateDoc(
+              doc(db, "users", String(report.reportedAccountId)),
+              {
+                permission: e.value,
+              }
+            );
             await getReportList();
             setEnableEdit(null);
           } catch (error) {
@@ -126,7 +150,7 @@ export default function ReportTable({ user }) {
       await getReportList();
     };
 
-    return () => getReports();
+    getReports();
   }, []);
 
   const toggleEdit = (e, id) => {
@@ -145,7 +169,7 @@ export default function ReportTable({ user }) {
       <div className="container table-container overflow-auto">
         <div className="org-table">
           <div className="flex flex-row justify-between items-center">
-            <h1 className="text-xl">HCP List</h1>
+            <h1 className="text-xl">Reports List</h1>
 
             <div className="right-side flex flex-row items-center gap-x-5">
               <div className="reload-list h-full">
@@ -175,7 +199,7 @@ export default function ReportTable({ user }) {
             <tbody>
               {reportList.length === 0
                 ? null
-                : reportList.map((report, index) => (/*hcp, index*/
+                : reportList.map((report, index /*hcp, index*/) => (
                     <tr key={index} className="fade-enter">
                       <td>
                         <span>
@@ -188,7 +212,8 @@ export default function ReportTable({ user }) {
                       </td>
                       <td>
                         <span>
-                          {report.reportDetails === "" || report.reportDetails === null
+                          {report.reportDetails === "" ||
+                          report.reportDetails === null
                             ? "Not Specified"
                             : report.reportDetails}
                         </span>
@@ -197,10 +222,16 @@ export default function ReportTable({ user }) {
                         {/* permission select input */}
                         <Select
                           options={options}
-                          isDisabled={enableEdit !== report.reportedAccountId ? true : false}
+                          isDisabled={
+                            enableEdit !== report.reportedAccountId
+                              ? true
+                              : false
+                          }
                           value={{
-                            value: report.permission,// options[2],
-                            label: report.permission[0].toUpperCase() + report.permission.slice(1),
+                            value: report.permission, // options[2],
+                            label:
+                              report.permission[0].toUpperCase() +
+                              report.permission.slice(1),
                           }}
                           onChange={(e) => handlePermissionChange(e, report)}
                         />
@@ -210,7 +241,9 @@ export default function ReportTable({ user }) {
                           <div className="hover:cursor-pointer">
                             <AiOutlineEdit
                               className="hover:cursor-pointer"
-                              onClick={(e) => toggleEdit(e, report.reportedAccountId)}
+                              onClick={(e) =>
+                                toggleEdit(e, report.reportedAccountId)
+                              }
                             />
                           </div>
                           <div className="hover:cursor-pointer">
