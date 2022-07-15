676f45b160accea458d354c9bd1316fb4e34f54d
tonylee221b <blackcoder787@gmail.com>
2022-07-15 13:08:09 -0400

    Thread Update with Auth fixed

--- client/components/forum/ThreadPreview.jsx
+++ client/components/forum/ThreadPreview.jsx
@@ -4,6 +4,7 @@ import { db } from "@/firebase/clientApp";
 import { getDoc, doc } from "firebase/firestore";
 import { useAuth } from "@/firebase/auth";
 import { useEffect, useState } from "react";
+import Loading from "../Loading";
 
 const ThreadPreview = ({ thread }) => {
   const date = new Date(Date(thread.createdBy)).toDateString();
@@ -30,52 +31,58 @@ const ThreadPreview = ({ thread }) => {
   // console.log(`in ThreadPreview 2: ${JSON.stringify(creatorData)}`);
 
   return (
-    <div className="bg-white mb-8 rounded-xl drop-shadow-lg border-2 border-gray-100">
-      <div className="flex rounded-xl p-2">
-        <div className="min-w-[150px]">
-          <img
-            src="https://via.placeholder.com/125"
-            width="150px"
-            height="150px"
-            className="p-4"
-          />
-          <div className="text-center">
-            {creatorData.firstName ? (
-              creatorData.firstName + " " + creatorData.lastName
-            ) : (
-              <div>Loading...</div>
-            )}
+    <>
+      {!isLoading ? (
+        <div className="bg-white mb-8 rounded-xl drop-shadow-lg border-2 border-gray-100">
+          <div className="flex rounded-xl p-2">
+            <div className="min-w-[150px]">
+              <img
+                src="https://via.placeholder.com/125"
+                width="150px"
+                height="150px"
+                className="p-4"
+              />
+              <div className="text-center">
+                {creatorData.firstName ? (
+                  creatorData.firstName + " " + creatorData.lastName
+                ) : (
+                  <div>Loading...</div>
+                )}
+              </div>
+            </div>
+            <div className="w-1/2 border-r border-gray-400 p-4">
+              <Link href={`/thread/${thread.id}`}>
+                <h2 className="text-2xl pb-2 hover:cursor-pointer hover:underline">
+                  {thread.title}
+                </h2>
+              </Link>
+              <p className="text-sm">{thread.desc}</p>
+              {user.uid != thread.authorId && (
+                <div className="follow-button inline-block align-middle">
+                  <ReportModal
+                    reportingThread={true}
+                    reportedUserData={creatorData /*getUserProfileData*/}
+                    reportedUserId={thread.authorId}
+                  ></ReportModal>
+                </div>
+              )}
+            </div>
+            <div className="p-4">
+              <div className="grow flex pb-4">
+                <div className="mr-7">
+                  <h4 className="text-gray-400">Replies</h4>
+                  <div className="text-center">{thread.replies.length}</div>
+                </div>
+              </div>
+              <h4 className="text-gray-400">Activity</h4>
+              <div>{date}</div>
+            </div>
           </div>
         </div>
-        <div className="w-1/2 border-r border-gray-400 p-4">
-          <Link href={`/thread/${thread.id}`}>
-            <h2 className="text-2xl pb-2 hover:cursor-pointer hover:underline">
-              {thread.title}
-            </h2>
-          </Link>
-          <p className="text-sm">{thread.desc}</p>
-          {user.uid != thread.authorId && (
-            <div className="follow-button inline-block align-middle">
-              <ReportModal
-                reportingThread={true}
-                reportedUserData={creatorData /*getUserProfileData*/}
-                reportedUserId={thread.authorId}
-              ></ReportModal>
-            </div>
-          )}
-        </div>
-        <div className="p-4">
-          <div className="grow flex pb-4">
-            <div className="mr-7">
-              <h4 className="text-gray-400">Replies</h4>
-              <div className="text-center">{thread.replies.length}</div>
-            </div>
-          </div>
-          <h4 className="text-gray-400">Activity</h4>
-          <div>{date}</div>
-        </div>
-      </div>
-    </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 };
 
